1b41ef1d36ed4474a27bfd6156eb6c6fb9129e12
Morten <mortenalbring@gmail.com>
2021-05-13 16:17:40 +0100

    Added Fade In Fade Out

--- fadeInFadeOut.bat
+++ fadeInFadeOut.bat
@@ -1,1 +1,3 @@
+:: https://video.stackexchange.com/questions/19867/how-to-fade-in-out-a-video-audio-clip-with-unknown-duration
+
 ffmpeg -i input.mp4 -sseof -1 -copyts -i input.mp4 -filter_complex "[1]fade=out:0:30[t];[0][t]overlay,fade=in:0:30[v];        anullsrc,atrim=0:2[at];[0][at]acrossfade=d=1,afade=d=1[a]" -map "[v]" -map "[a]" -c:v libx264 -crf 22 -preset veryfast -shortest fadeInOut.mp4
